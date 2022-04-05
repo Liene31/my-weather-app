4092bca7f61257c9088cfd9c578d9b41cecb559e
Liene31 <lienebare@gmail.com>
2022-04-05 20:28:57 +0200

    Removed 'visibility' and replaced by 'description'

--- src/index.js
+++ src/index.js
@@ -38,8 +38,8 @@ function updateCurrentMeteo(response) {
 
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;  
 
-  document.querySelector("#visibility").innerHTML = response.data.visibility;   
-
+  document.querySelector("#description").innerHTML = response.data.weather[0].description;  
+  
   document.querySelector("#wind-speed").innerHTML = Math.round(response.data.wind.speed);  
 
   document.querySelector("#city").innerHTML = response.data.name;       
